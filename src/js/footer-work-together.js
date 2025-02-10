152aa48b1966bf8c21687b13fadeec1d08a5b3dd
Alina Komerysta <alinakomerista3@gmail.com>
2025-02-10 11:46:11 +0100

    add modal window

---
+++ src/js/footer-work-together.js
@@ -0,0 +1,42 @@
+ console.log('footer')
+document.getElementById('form-inf').addEventListener('submit', function (event) {
+     event.preventDefault();
+
+    const formData = {
+      email: document.getElementById('user-email').value.trim(),
+      comment: document.getElementById('user-comment').value.trim()
+    };
+    fetch('https://portfolio-js.b.goit.study/api/requests', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+  
+.then(response => response.json())
+    .then(data => {
+        document.getElementById('form-inf').reset();
+        openModal('footer-modal');
+    })
+    .catch(error => console.error('Error submitting form:', error));
+});
+function openModal(modalId) {
+       const modal = document.getElementById(modalId);
+
+ if (modal) {
+    console.log('Opening modal:', modalId);
+    modal.style.display = 'flex';  // Відкриває модальне вікно
+  } else {
+    console.error('Modal not found:', modalId);  // Якщо модального вікна немає
+  }  }
+
+  document.getElementById('close-btn').addEventListener('click', function () {
+    document.getElementById('footer-modal').style.display = 'none';
+  });
+
+  window.addEventListener('click', function (event) {
+    if (event.target.id === 'footer-modal') {
+      document.getElementById('footer-modal').style.display = 'none';
+    }
+  });
